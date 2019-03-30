071d59afd57c4bb8d800e469442a6c57b72130ff
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-30 14:50:54 -0700

    重命名

--- src/文本编辑器/文本编辑器.java
+++ src/文本编辑器/文本编辑器.java
@@ -28,161 +28,161 @@ import javax.swing.text.DefaultEditorKit;
 
 public class 文本编辑器 extends JFrame {
 
-  private JTextArea area = new JTextArea(20, 120);
-  private JFileChooser dialog = new JFileChooser(System.getProperty("user.dir"));
-  private String currentFile = "Untitled";
-  private boolean changed = false;
+  private JTextArea 区域 = new JTextArea(20, 120);
+  private JFileChooser 对话框 = new JFileChooser(System.getProperty("user.dir"));
+  private String 当前文件 = "Untitled";
+  private boolean 已改 = false;
 
-  private KeyListener k1 = new KeyAdapter() {
+  private KeyListener 监听按键 = new KeyAdapter() {
     @Override
     public void keyPressed(KeyEvent e) {
-      changed = true;
-      Save.setEnabled(true);
-      SaveAs.setEnabled(true);
+      已改 = true;
+      保存.setEnabled(true);
+      另存为.setEnabled(true);
     }
   };
   Action New = new AbstractAction("New", new ImageIcon("Path/image.gif")) {
     @Override
     public void actionPerformed(ActionEvent e) {
-      saveOld();
-      area.setText("");
-      currentFile = "Untitled";
-      setTitle(currentFile);
-      changed = false;
-      Save.setEnabled(false);
-      SaveAs.setEnabled(false);
+      保存旧文件();
+      区域.setText("");
+      当前文件 = "Untitled";
+      setTitle(当前文件);
+      已改 = false;
+      保存.setEnabled(false);
+      另存为.setEnabled(false);
     }
   };
-  Action Open = new AbstractAction("Open", new ImageIcon("open.gif")) {
+  Action 打开 = new AbstractAction("Open", new ImageIcon("open.gif")) {
     @Override
     public void actionPerformed(ActionEvent e) {
-      saveOld();
-      if (dialog.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-        readInFile(dialog.getSelectedFile().getAbsolutePath());
+      保存旧文件();
+      if (对话框.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+        读入文件(对话框.getSelectedFile().getAbsolutePath());
       }
-      SaveAs.setEnabled(true);
+      另存为.setEnabled(true);
     }
   };
-  Action Save = new AbstractAction("Save", new ImageIcon("save.gif")) {
+  Action 保存 = new AbstractAction("Save", new ImageIcon("save.gif")) {
     @Override
     public void actionPerformed(ActionEvent e) {
-      if (!currentFile.equals("Untitled"))
-        saveFile(currentFile);
+      if (!当前文件.equals("Untitled"))
+        保存文件(当前文件);
       else
-        saveFileAs();
+        另存文件为();
     }
   };
-  Action SaveAs = new AbstractAction("Save as...") {
+  Action 另存为 = new AbstractAction("Save as...") {
     @Override
     public void actionPerformed(ActionEvent e) {
-      saveFileAs();
+      另存文件为();
     }
   };
-  Action Quit = new AbstractAction("Quit") {
+  Action 退出 = new AbstractAction("Quit") {
     @Override
     public void actionPerformed(ActionEvent e) {
-      saveOld();
+      保存旧文件();
       System.exit(0);
     }
   };
-  ActionMap m = area.getActionMap();
-  Action Cut = m.get(DefaultEditorKit.cutAction);
-  Action Copy = m.get(DefaultEditorKit.copyAction);
-  Action Paste = m.get(DefaultEditorKit.pasteAction);
+  ActionMap 操作表 = 区域.getActionMap();
+  Action 剪切 = 操作表.get(DefaultEditorKit.cutAction);
+  Action 拷贝 = 操作表.get(DefaultEditorKit.copyAction);
+  Action 粘贴 = 操作表.get(DefaultEditorKit.pasteAction);
 
   public 文本编辑器() {
-    area.setFont(new Font("Monospaced", Font.PLAIN, 12));
-    JScrollPane scroll = new JScrollPane(area, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+    区域.setFont(new Font("Monospaced", Font.PLAIN, 12));
+    JScrollPane 滚动条 = new JScrollPane(区域, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
         JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-    add(scroll, BorderLayout.CENTER);
+    add(滚动条, BorderLayout.CENTER);
 
     JMenuBar 菜单栏 = new JMenuBar();
     setJMenuBar(菜单栏);
-    JMenu file = new JMenu("File");
-    JMenu edit = new JMenu("Edit");
-    菜单栏.add(file);
-    菜单栏.add(edit);
+    JMenu 文件菜单 = new JMenu("File");
+    JMenu 编辑菜单 = new JMenu("Edit");
+    菜单栏.add(文件菜单);
+    菜单栏.add(编辑菜单);
 
-    file.add(New);
-    file.add(Open);
-    file.add(Save);
-    file.add(Quit);
-    file.add(SaveAs);
-    file.addSeparator();
+    文件菜单.add(New);
+    文件菜单.add(打开);
+    文件菜单.add(保存);
+    文件菜单.add(退出);
+    文件菜单.add(另存为);
+    文件菜单.addSeparator();
 
     for (int i = 0; i < 4; i++)
-      file.getItem(i).setIcon(null);
+      文件菜单.getItem(i).setIcon(null);
 
-    edit.add(Cut);
-    edit.add(Copy);
-    edit.add(Paste);
+    编辑菜单.add(剪切);
+    编辑菜单.add(拷贝);
+    编辑菜单.add(粘贴);
 
-    edit.getItem(0).setText("Cut out");
-    edit.getItem(1).setText("Copy");
-    edit.getItem(2).setText("Paste");
+    编辑菜单.getItem(0).setText("Cut out");
+    编辑菜单.getItem(1).setText("Copy");
+    编辑菜单.getItem(2).setText("Paste");
 
-    JToolBar tool = new JToolBar();
-    add(tool, BorderLayout.NORTH);
-    tool.add(New);
-    tool.add(Open);
-    tool.add(Save);
-    tool.addSeparator();
+    JToolBar 工具栏 = new JToolBar();
+    add(工具栏, BorderLayout.NORTH);
+    工具栏.add(New);
+    工具栏.add(打开);
+    工具栏.add(保存);
+    工具栏.addSeparator();
 
-    JButton cut = tool.add(Cut), cop = tool.add(Copy), pas = tool.add(Paste);
+    JButton 剪切按钮 = 工具栏.add(剪切), 拷贝按钮 = 工具栏.add(拷贝), 粘贴按钮 = 工具栏.add(粘贴);
 
-    cut.setText(null);
-    cut.setIcon(new ImageIcon("cut.gif"));
-    cop.setText(null);
-    cop.setIcon(new ImageIcon("copy.gif"));
-    pas.setText(null);
-    pas.setIcon(new ImageIcon("paste.gif"));
+    剪切按钮.setText(null);
+    剪切按钮.setIcon(new ImageIcon("cut.gif"));
+    拷贝按钮.setText(null);
+    拷贝按钮.setIcon(new ImageIcon("copy.gif"));
+    粘贴按钮.setText(null);
+    粘贴按钮.setIcon(new ImageIcon("paste.gif"));
 
-    Save.setEnabled(false);
-    SaveAs.setEnabled(false);
+    保存.setEnabled(false);
+    另存为.setEnabled(false);
 
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     pack();
-    area.addKeyListener(k1);
-    setTitle(currentFile);
+    区域.addKeyListener(监听按键);
+    setTitle(当前文件);
     setVisible(true);
   }
 
-  private void saveFileAs() {
-    if (dialog.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
-      saveFile(dialog.getSelectedFile().getAbsolutePath());
+  private void 另存文件为() {
+    if (对话框.showSaveDialog(null) == JFileChooser.APPROVE_OPTION)
+      保存文件(对话框.getSelectedFile().getAbsolutePath());
   }
 
-  private void saveOld() {
-    if (changed) {
-      if (JOptionPane.showConfirmDialog(this, "Would you like to save " + currentFile + " ?",
+  private void 保存旧文件() {
+    if (已改) {
+      if (JOptionPane.showConfirmDialog(this, "Would you like to save " + 当前文件 + " ?",
           "Save", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
-        saveFile(currentFile);
+        保存文件(当前文件);
     }
   }
 
-  private void readInFile(String fileName) {
+  private void 读入文件(String 文件名) {
     try {
-      FileReader r = new FileReader(fileName);
-      area.read(r, null);
-      r.close();
-      currentFile = fileName;
-      setTitle(currentFile);
-      changed = false;
+      FileReader 读 = new FileReader(文件名);
+      区域.read(读, null);
+      读.close();
+      当前文件 = 文件名;
+      setTitle(当前文件);
+      已改 = false;
     } catch (IOException e) {
       Toolkit.getDefaultToolkit().beep();
-      JOptionPane.showMessageDialog(this, "Editor can't find the file called " + fileName);
+      JOptionPane.showMessageDialog(this, "Editor can't find the file called " + 文件名);
     }
   }
 
-  private void saveFile(String fileName) {
+  private void 保存文件(String 文件名) {
     try {
-      FileWriter w = new FileWriter(fileName);
-      area.write(w);
-      w.close();
-      currentFile = fileName;
-      setTitle(currentFile);
-      changed = false;
-      Save.setEnabled(false);
+      FileWriter 写 = new FileWriter(文件名);
+      区域.write(写);
+      写.close();
+      当前文件 = 文件名;
+      setTitle(当前文件);
+      已改 = false;
+      保存.setEnabled(false);
     } catch (IOException e) {
     }
   }
