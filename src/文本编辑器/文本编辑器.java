bbe49c28fc022660fd55f59165f6ed988c92889c
fromwheretowhere <fromwheretowhere.service@gmail.com>
2019-03-30 17:25:19 -0700

    全部界面操作改为对应命令

--- src/文本编辑器/文本编辑器.java
+++ src/文本编辑器/文本编辑器.java
@@ -19,8 +19,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -33,12 +31,18 @@ public class 文本编辑器 extends JFrame {
   private static final String 图标目录 = "图标/";
   private static final String 默认文件名 = "无名";
 
-  private JTextArea 区域 = new JTextArea(20, 120);
+  private JTextArea 文本区域 = new JTextArea(20, 120);
 
-  private JTextArea 命令 = new JTextArea(20, 20);
+  private JTextArea 命令文本区 = new JTextArea(20, 20);
   private JFileChooser 对话框 = new JFileChooser(System.getProperty("user.dir"));
   private String 当前文件 = 默认文件名;
+  private String 当前命令 = "";
   private boolean 已改 = false;
+
+  ActionMap 操作表 = 文本区域.getActionMap();
+  Action 剪切 = 操作表.get(DefaultEditorKit.cutAction);
+  Action 拷贝 = 操作表.get(DefaultEditorKit.copyAction);
+  Action 粘贴 = 操作表.get(DefaultEditorKit.pasteAction);
 
   private KeyListener 监听按键 = new KeyAdapter() {
     @Override
@@ -54,54 +58,99 @@ public class 文本编辑器 extends JFrame {
     public void keyPressed(KeyEvent e) {
       String 此行 = "";
       if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-        int 行数 = 命令.getLineCount();
+        int 行数 = 命令文本区.getLineCount();
         if (行数 == 1) {
-          此行 = 命令.getText();
+          此行 = 命令文本区.getText();
         } else {
           int 上一行末位置 = 0;
           try {
-            上一行末位置 = 命令.getLineEndOffset(行数 - 2);
+            上一行末位置 = 命令文本区.getLineEndOffset(行数 - 2);
           } catch (BadLocationException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
           }
-          此行 = 命令.getText().substring(上一行末位置);
+          此行 = 命令文本区.getText().substring(上一行末位置);
         }
-        System.out.println(此行);
+        当前命令 = 此行;
+        switch (当前命令) {
+          case "新建":
+            新建操作();
+            break;
+          case "打开":
+            打开操作();
+            break;
+          case "保存":
+            保存操作();
+            break;
+          case "另存":
+            另存文件为();
+            break;
+          case "退出":
+            退出操作();
+            break;
+          case "剪切":
+            文本区域.cut();
+            break;
+          case "复制":
+            文本区域.copy();
+            break;
+          case "粘贴":
+            文本区域.paste();
+            break;
+          default:
+            // TODO: 提示无此命令或显示帮助
+            break;
+        }
       }
     }
   };
+
   Action 新建 = new AbstractAction("新建", new ImageIcon(图标目录 + "new.gif")) {
     @Override
     public void actionPerformed(ActionEvent e) {
-      保存旧文件();
-      区域.setText("");
-      当前文件 = 默认文件名;
-      setTitle(当前文件);
-      已改 = false;
-      保存.setEnabled(false);
-      另存为.setEnabled(false);
+      新建操作();
     }
   };
+
+  private void 新建操作() {
+    保存旧文件();
+    文本区域.setText("");
+    当前文件 = 默认文件名;
+    setTitle(当前文件);
+    已改 = false;
+    保存.setEnabled(false);
+    另存为.setEnabled(false);
+  }
+
   Action 打开 = new AbstractAction("打开", new ImageIcon(图标目录 + "open.gif")) {
     @Override
     public void actionPerformed(ActionEvent e) {
-      保存旧文件();
-      if (对话框.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-        读入文件(对话框.getSelectedFile().getAbsolutePath());
-      }
-      另存为.setEnabled(true);
+      打开操作();
     }
   };
+
+  private void 打开操作() {
+    保存旧文件();
+    if (对话框.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+      读入文件(对话框.getSelectedFile().getAbsolutePath());
+    }
+    另存为.setEnabled(true);
+  }
+
   Action 保存 = new AbstractAction("保存", new ImageIcon(图标目录 + "save.gif")) {
     @Override
     public void actionPerformed(ActionEvent e) {
-      if (!当前文件.equals(默认文件名))
-        保存文件(当前文件);
-      else
-        另存文件为();
+      保存操作();
     }
   };
+
+  private void 保存操作() {
+    if (!当前文件.equals(默认文件名))
+      保存文件(当前文件);
+    else
+      另存文件为();
+  }
+
   Action 另存为 = new AbstractAction("另存为...") {
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -111,27 +160,27 @@ public class 文本编辑器 extends JFrame {
   Action 退出 = new AbstractAction("退出") {
     @Override
     public void actionPerformed(ActionEvent e) {
-      保存旧文件();
-      System.exit(0);
+      退出操作();
     }
   };
-  ActionMap 操作表 = 区域.getActionMap();
-  Action 剪切 = 操作表.get(DefaultEditorKit.cutAction);
-  Action 拷贝 = 操作表.get(DefaultEditorKit.copyAction);
-  Action 粘贴 = 操作表.get(DefaultEditorKit.pasteAction);
+
+  private void 退出操作() {
+    保存旧文件();
+    System.exit(0);
+  }
 
   public 文本编辑器() {
     setLayout(new GridLayout(2, 1));
-    区域.setFont(new Font("Monospaced", Font.PLAIN, 12));
-    JScrollPane 文本区 = new JScrollPane(区域, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+    文本区域.setFont(new Font("Monospaced", Font.PLAIN, 12));
+    JScrollPane 文本区 = new JScrollPane(文本区域, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
         JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     add(文本区, BorderLayout.CENTER);
 
-    命令.setFont(new Font("Monospaced", Font.PLAIN, 12));
-    JScrollPane 命令区 = new JScrollPane(命令, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+    命令文本区.setFont(new Font("Monospaced", Font.PLAIN, 12));
+    JScrollPane 命令区 = new JScrollPane(命令文本区, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
         JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     add(命令区);
-
+/*
     JMenuBar 菜单栏 = new JMenuBar();
     setJMenuBar(菜单栏);
     JMenu 文件菜单 = new JMenu("文件");
@@ -156,7 +205,7 @@ public class 文本编辑器 extends JFrame {
     编辑菜单.getItem(0).setText("剪切");
     编辑菜单.getItem(1).setText("拷贝");
     编辑菜单.getItem(2).setText("粘贴");
-
+*/
     JToolBar 工具栏 = new JToolBar();
     add(工具栏, BorderLayout.NORTH);
     工具栏.add(新建);
@@ -178,8 +227,8 @@ public class 文本编辑器 extends JFrame {
 
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     pack();
-    区域.addKeyListener(监听按键);
-    命令.addKeyListener(命令监听);
+    文本区域.addKeyListener(监听按键);
+    命令文本区.addKeyListener(命令监听);
     setTitle(当前文件);
     setVisible(true);
   }
@@ -200,7 +249,7 @@ public class 文本编辑器 extends JFrame {
   private void 读入文件(String 文件名) {
     try {
       FileReader 读 = new FileReader(文件名);
-      区域.read(读, null);
+      文本区域.read(读, null);
       读.close();
       当前文件 = 文件名;
       setTitle(当前文件);
@@ -214,7 +263,7 @@ public class 文本编辑器 extends JFrame {
   private void 保存文件(String 文件名) {
     try {
       FileWriter 写 = new FileWriter(文件名);
-      区域.write(写);
+      文本区域.write(写);
       写.close();
       当前文件 = 文件名;
       setTitle(当前文件);
